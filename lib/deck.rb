582462a820f39af026cade69ce76a967d13e0c17
Nick Spencer <strangebrew202@gmail.com>
2023-07-03 18:45:37 -0600

    Add 4 methods to deck.rb and test cases for those methods in deck_spec.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,4 +8,22 @@ class Deck
     def rank_of_card_at(index)
         @cards[index].rank
     end
+
+    def high_ranking_cards
+        high_cards = @cards.reject do |card|
+            card.rank < 10
+        end
+    end
+
+    def percent_high_ranking
+        percent = (Float(high_ranking_cards.count) / Float(@cards.count))
+    end
+
+    def remove_card
+        removed_card = @cards.shift
+    end
+
+    def add_card(card)
+        @cards << card
+    end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -7,23 +7,43 @@ RSpec.describe Deck do
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
     it "exists" do
-        deck = Deck.new(cards)
-
         expect(deck).to be_an_instance_of(Deck)
     end
 
     it "can read cards" do
-        deck = Deck.new(cards)
-
         expect(deck.cards).to eq(cards)
     end
 
     it "can read specific cards" do 
-        deck = Deck.new(cards)
-
         expect(deck.rank_of_card_at(0)).to eq(12)
         expect(deck.rank_of_card_at(2)).to eq(14)
+    end
+
+    it "can return high ranking cards" do 
+        expect(deck.high_ranking_cards).to eq([card1, card3])
+    end
+
+    it "can calculate percent of high cards in deck" do
+        expect(deck.percent_high_ranking).to eq(2.0 / 3.0)
+    end
+
+    it "can remove a card" do 
+        deck.remove_card
+
+        expect(deck.cards).to eq([card2, card3])
+        expect(deck.high_ranking_cards).to eq([card3])
+        expect(deck.percent_high_ranking).to eq(0.5)
+    end
+
+    it "can add a card" do
+        card4 = Card.new(:club, '5', 5)
+        deck.add_card(card4)
+
+        expect(deck.cards).to eq([card2, card3, card4])
+        expect(deck.high_ranking_cards).to eq([card3])
+        expect(deck.percent_high_ranking).to eq(1.0/3.0)
     end
 end
